520854de667907714e6b42e8ada441d87512c8c1
Rashmi <rashmijayasekara00@gmail.com>
2023-01-07 11:13:00 +0530

    Set up the Database Connectivity

---
+++ src/main/java/lk/ijse/employeemanager/model/Employee.java
@@ -0,0 +1,24 @@
+package lk.ijse.employeemanager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data @AllArgsConstructor @NoArgsConstructor @Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false,updatable = false)
+    private int id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false,updatable = false)
+    private String employeeCode;
+}
